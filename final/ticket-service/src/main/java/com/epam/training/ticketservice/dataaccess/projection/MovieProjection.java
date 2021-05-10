ad5e2de018369245b214b56f5b51d29373a2cb1b
Duszka Ákos Attila <43644528+duszka-akos-attila@users.noreply.github.com>
2021-05-10 03:59:16 +0200

    Update MovieProjection.java

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/dataaccess/projection/MovieProjection.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/dataaccess/projection/MovieProjection.java
@@ -1,14 +1,10 @@
 package com.epam.training.ticketservice.dataaccess.projection;
 
-import com.epam.training.ticketservice.dataaccess.projection.compositeKey.MovieCompositeKey;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,10 +14,13 @@ public class MovieProjection {
 
     @Id
     @GeneratedValue
-    private long movieId;
+    @Column(name = "id")
+    private Long movieId;
 
-    @EmbeddedId
-    private MovieCompositeKey movieCompositeKey;
+    @Column(unique = true)
+    private String movieTitle;
+
+    private String movieGenre;
 
     private int movieLength;
 }
